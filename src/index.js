a204f07c56518e690114639eb89893bd63929845
amitzed <amitzmn@gmail.com>
2020-06-23 11:26:22 -0400

    Installed axios to communicate with api

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 
 import App from './components/App';
 
+const KEY = process.env.REACT_APP_YOUTUBE_API_KEY
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
