659934521c3edd92c288c72ddd0b57718b6bdd02
amitzed <amitzmn@gmail.com>
2020-06-23 11:31:15 -0400

    Hooked up api to App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,21 @@
 import React from 'react';
 
+import youtube from '../apis/youtube.js';
+
+const KEY = process.env.REACT_APP_YOUTUBE_API_KEY
+
 class App extends React.Component {
+  onTextSubmit = (term) => {
+    youtube.get("/search", {
+      params: {
+        q: term,
+        part: "snippet",
+        maxResults: 50,
+        key: KEY
+      }
+    });
+  }
+
   render() {
     return (
       <div>
--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 
 import App from './components/App';
 
-const KEY = process.env.REACT_APP_YOUTUBE_API_KEY
-
 ReactDOM.render(
   <App />,
   document.getElementById('root')
