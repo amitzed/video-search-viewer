88e67f04dcfdac2bdb4caa2bce4c0710a10cc6eb
amitzed <amitzmn@gmail.com>
2020-06-25 22:33:18 -0400

    Thumbnails for each item now showing up along with title

--- src/components/Item.js
+++ src/components/Item.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 const Item = ( {video} ) => {
   return (
-    <div>{video.snippet.title}</div>
+    <div>
+      {video.snippet.title}
+      <img src={video.snippet.thumbnails.medium.url} />
+    </div>
   )
 }
 
