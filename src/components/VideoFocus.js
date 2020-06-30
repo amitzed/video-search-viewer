6c51ca952e4dc6534d7209a518c7d24ca9152812
amitzed <amitzmn@gmail.com>
2020-06-30 18:37:01 -0400

    In VideoFocus.js added an iFrame element to reference videoSource variable and now on click, selected video shows in main area

--- src/components/VideoFocus.js
+++ src/components/VideoFocus.js
@@ -13,7 +13,9 @@ const VideoFocus = ( {video}) => {
 
   return (
     <div className="card">
-      <img src="https://tbivision.com/files/2020/05/TikTok.png" className="card-img-top" alt="An Image" />
+      <div className="card-img-top">
+        <iframe src={videoSource} />
+      </div>
       <div className="card-body">
         <h5 className="card-title">{video.snippet.title}</h5>
         <p className="card-text">{video.snippet.description}</p>
