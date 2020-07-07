113ceb7c988e9b5ee9973f95ab856f302b22706f
amitzed <amitzmn@gmail.com>
2020-07-06 20:40:03 -0400

    Applied some initial styles for smaller screens but larger than iPad

--- src/components/Item.js
+++ src/components/Item.js
@@ -9,7 +9,7 @@ const Item = ( {video, onVideoSelect} ) => {
         </div>
       </div>
       <hr className="mx-3" />
-      <div className="col-md-4">
+      <div className="col-md-4 item-individual">
         <img src={video.snippet.thumbnails.medium.url} alt={video.snippet.title} />
       </div>
     </div>
--- src/components/partials/Header.js
+++ src/components/partials/Header.js
@@ -12,7 +12,7 @@ export const Header = () => {
         <h1 className="display-4" style={{textAlign: 'center'}}>
           <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-skip-backward" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
             <path fill-rule="evenodd" d="M.5 3.5A.5.5 0 0 1 1 4v3.248l6.267-3.636c.52-.302 1.233.043 1.233.696v2.94l6.267-3.636c.52-.302 1.233.043 1.233.696v7.384c0 .653-.713.998-1.233.696L8.5 8.752v2.94c0 .653-.713.998-1.233.696L1 8.752V12a.5.5 0 0 1-1 0V4a.5.5 0 0 1 .5-.5zm7 1.133L1.696 8 7.5 11.367V4.633zm7.5 0L9.196 8 15 11.367V4.633z"/>
-          </svg>            
+          </svg>
           <i className="header-title">Video Shakedown</i>
           <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-skip-forward" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
           <path fill-rule="evenodd" d="M15.5 3.5a.5.5 0 0 1 .5.5v8a.5.5 0 0 1-1 0V8.752l-6.267 3.636c-.52.302-1.233-.043-1.233-.696v-2.94l-6.267 3.636C.713 12.69 0 12.345 0 11.692V4.308c0-.653.713-.998 1.233-.696L7.5 7.248v-2.94c0-.653.713-.998 1.233-.696L15 7.248V4a.5.5 0 0 1 .5-.5zM1 4.633v6.734L6.804 8 1 4.633zm7.5 0v6.734L14.304 8 8.5 4.633z"/>
